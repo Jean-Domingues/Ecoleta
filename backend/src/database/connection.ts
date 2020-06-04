ad0936e92450bd1438a9efbcefe5be9e776eae88
Jean Domingues <jeandomingues752@gmail.com>
2020-06-04 12:13:35 -0300

    Backend

---
+++ backend/src/database/connection.ts
@@ -0,0 +1,13 @@
+import knex from 'knex';
+
+import path from 'path';
+
+const connetion = knex({
+  client: 'sqlite3',
+  connection: {
+    filename: path.resolve(__dirname, 'database.sqlite'),
+  },
+  useNullAsDefault: true,
+});
+
+export default connetion;
