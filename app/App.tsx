bb0daa7a149a7234c93a20aff6929bb3b49885fa
Jean Domingues <jeandomingues752@gmail.com>
2020-07-20 15:13:09 -0300

    pasta app criada

---
+++ app/App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'react-native';
+import React from 'react';
+
+import Home from './src/pages/Home';
+
+export default function App() {
+  return (
+    <>
+    <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent/>
+    <Home/>
+   </>
+  );
+}
