e2514e137c6631ce9ed8ef74533f4e302f4d293a
Jean Domingues <jeandomingues752@gmail.com>
2020-07-22 16:45:04 -0300

    ulploaded de imagens funcionando

--- frontend/src/components/Dropzone/index.tsx
+++ frontend/src/components/Dropzone/index.tsx
@@ -3,8 +3,13 @@ import { useDropzone } from 'react-dropzone'
 import { FiUpload } from 'react-icons/fi';
 
 import './styles.css';
+import { inflate } from 'zlib';
 
-const Dropzone = () => {
+interface Props {
+  onFileUploaded: (file: File) => void;
+}
+
+const Dropzone: React.FC<Props> = ({ onFileUploaded }) => {
   const [selectedFileUrl, setSelectedFileUrl] = useState('');
 
   const onDrop = useCallback(acceptedFiles => {
@@ -12,7 +17,8 @@ const Dropzone = () => {
 
     const fileUrl = URL.createObjectURL(file);
     setSelectedFileUrl(fileUrl);
-  }, [])
+    onFileUploaded(file);
+  }, [onFileUploaded])
 
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
@@ -23,13 +29,13 @@ const Dropzone = () => {
     <div className="dropzone" {...getRootProps()}>
       <input {...getInputProps()} accept="image/*" />
 
-      { selectedFileUrl ? <img src={selectedFileUrl} alt="Point Thumbnail" />
-          : (
-            <p>
-              <FiUpload />
+      {selectedFileUrl ? <img src={selectedFileUrl} alt="Point Thumbnail" />
+        : (
+          <p>
+            <FiUpload />
               Imagem do estabelecimento
-            </p>
-          )}
+          </p>
+        )}
     </div>
   )
 }
